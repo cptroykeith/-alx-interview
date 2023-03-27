9e2301f45234d2d0603bcaed496f1954aa5e4030
your-name <katongole.roy100@gmail.com>
2023-03-27 19:35:11 +0300

    complete

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-
-"""
-    Interview prepration exercise: Lock Boxes
-"""
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """
-        A method that determines if all boxes can be opened.
-        Boxes is a list of lists.
-        A key with the same number as a box opens that box.
-        You can assume all keys will be positive integers.
-        There can be keys that do not have boxes.
-        The first box boxes[0] is unlocked.
-        Return True if all boxes can be opened, else return False.
-    """
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
     length = len(boxes)
     keys = set()
-    opened = []
+    opened_boxes = []
     i = 0
 
     while i < length:
-        temp = i
-        opened.append(i)
+        oldi = i
+        opened_boxes.append(i)
         keys.update(boxes[i])
         for key in keys:
-            if key != 0 and key < length and key not in opened:
+            if key != 0 and key < length and key not in opened_boxes:
                 i = key
                 break
-        if temp == i:
+        if oldi != i:
+            continue
+        else:
             break
 
     for i in range(length):
-        if i not in opened and i != 0:
+        if i not in opened_boxes and i != 0:
             return False
     return True
